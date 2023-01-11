5b61e1e0544c3e253b32843865319b343339ebc2
Maciek Kamiński <aundumla@gmail.com>
2023-01-11 18:51:47 +0100

    start both forked chains

---
+++ hardhat.config.ts
@@ -0,0 +1,48 @@
+import "@nomiclabs/hardhat-ethers";
+import "@shardlabs/starknet-hardhat-plugin";
+
+const config = {
+  defaultNetwork: "hardhat",
+  networks: {
+    goerli: {
+      url: "http://127.0.0.1:8545",
+    },
+    mainnet: {
+      url: "http://127.0.0.1:8545",
+    },
+    devnet: {
+      //starknet devnet endpoint
+      url: "http://127.0.0.1:5050",
+    },
+  },
+  starknet: {
+    dockerizedVersion: "0.10.3",
+    network: "devnet",
+  },
+  paths: {
+    artifacts: "./artifacts",
+    cache: "./cache",
+    sources: "./src",
+    tests: "./src",
+    starknetSources: "./src",
+    starknetArtifacts: "./starknet-artifacts",
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.14",
+        settings: {
+          metadata: {
+            bytecodeHash: "none",
+          },
+          optimizer: {
+            enabled: true,
+            runs: 800,
+          },
+        },
+      },
+    ],
+  },
+};
+
+export default config;
---
+++ scripts/node.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+hardhat node --fork https://$1.infura.io/v3/56387818e18e404a9a6d2391af0e9085
