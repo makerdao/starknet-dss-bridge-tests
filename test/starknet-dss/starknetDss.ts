820bb0b6a97b7dfdd3607345f75dfeecd9eaa424
Evolve <Artevolve@yahoo.com>
2023-01-27 14:40:36 +0100

    feat: add starknet prank ✨

--- test/starknet-dss/starknetDss.ts
+++ test/starknet-dss/starknetDss.ts
@@ -1,17 +1,25 @@
-import {wrap, WrappedStarknetContract, wrapTyped} from "../helpers/starknet/wrap";
+import {
+	wrap,
+	WrappedStarknetContract,
+	wrapTyped,
+} from "../helpers/starknet/wrap";
 import vatAbi from "./abi/vatAbi";
 import hre from "hardhat";
-import {Account} from "@shardlabs/starknet-hardhat-plugin/dist/src/account";
+import { Account } from "@shardlabs/starknet-hardhat-plugin/dist/src/account";
+import { starknetPrank } from "../helpers/starknet/prank";
 
 type SNVat = WrappedStarknetContract<typeof vatAbi>;
 type Felt = bigint | string; // TODO: what it should be exactly
 
 // TODO: there should be global "active account" ala prank somewhere
-export async function deploySNVat(deployer: Account, ward: Felt): Promise<SNVat> {
-  const factory = await hre.starknet.getContractFactory("vat");
-  await deployer.declare(factory);
-  const vat = await deployer.deploy(factory, { ward })
-  return wrapTyped(hre, vat);
+export async function deploySNVat(
+	deployer: Account,
+	ward: Felt
+): Promise<SNVat> {
+	const factory = await hre.starknet.getContractFactory("vat");
+	await deployer.declare(factory);
+	const vat = await deployer.deploy(factory, { ward });
+	return starknetPrank<typeof vatAbi>(wrapTyped(hre, vat));
 }
 
 // TODO: add more contracts
