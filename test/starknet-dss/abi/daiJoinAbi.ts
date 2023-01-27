eaabb9845144a40df5b21ac4dabcc6b70a100f5f
Evolve <Artevolve@yahoo.com>
2023-01-27 15:09:31 +0100

    feat: contracts getters ✨

---
+++ test/starknet-dss/abi/daiJoinAbi.ts
@@ -0,0 +1,94 @@
+export default [
+	{
+		members: [
+			{
+				name: "low",
+				offset: 0,
+				type: "felt",
+			},
+			{
+				name: "high",
+				offset: 1,
+				type: "felt",
+			},
+		],
+		name: "Uint256",
+		size: 2,
+		type: "struct",
+	},
+	{
+		data: [
+			{
+				name: "usr",
+				type: "felt",
+			},
+			{
+				name: "wad",
+				type: "Uint256",
+			},
+		],
+		keys: [],
+		name: "Join",
+		type: "event",
+	},
+	{
+		data: [
+			{
+				name: "usr",
+				type: "felt",
+			},
+			{
+				name: "wad",
+				type: "Uint256",
+			},
+		],
+		keys: [],
+		name: "Exit",
+		type: "event",
+	},
+	{
+		inputs: [
+			{
+				name: "vat",
+				type: "felt",
+			},
+			{
+				name: "dai",
+				type: "felt",
+			},
+		],
+		name: "constructor",
+		outputs: [],
+		type: "constructor",
+	},
+	{
+		inputs: [
+			{
+				name: "usr",
+				type: "felt",
+			},
+			{
+				name: "wad",
+				type: "Uint256",
+			},
+		],
+		name: "join",
+		outputs: [],
+		type: "function",
+	},
+	{
+		inputs: [
+			{
+				name: "usr",
+				type: "felt",
+			},
+			{
+				name: "wad",
+				type: "Uint256",
+			},
+		],
+		name: "exit",
+		outputs: [],
+		type: "function",
+	},
+] as const;
--- test/starknet-dss/starknetDss.ts
+++ test/starknet-dss/starknetDss.ts
@@ -3,13 +3,25 @@ import {
 	WrappedStarknetContract,
 	wrapTyped,
 } from "../helpers/starknet/wrap";
-import vatAbi from "./abi/vatAbi";
 import hre from "hardhat";
 import { Account } from "@shardlabs/starknet-hardhat-plugin/dist/src/account";
 import { starknetPrank } from "../helpers/starknet/prank";
+import { SplitUintType } from "../../lib/starknet-dss/test/utils";
 
-type SNVat = WrappedStarknetContract<typeof vatAbi>;
-type Felt = bigint | string; // TODO: what it should be exactly
+import vatAbi from "./abi/vatAbi";
+import daiAbi from "./abi/daiAbi";
+import cureAbi from "./abi/cureAbi";
+import daiJoinAbi from "./abi/daiJoinAbi";
+import jugAbi from "./abi/jugAbi";
+import { getL2ContractAt } from "../helpers/starknet/utils";
+
+export type SNVat = WrappedStarknetContract<typeof vatAbi>;
+export type SNDai = WrappedStarknetContract<typeof daiAbi>;
+export type SNCure = WrappedStarknetContract<typeof cureAbi>;
+export type SNDaiJoin = WrappedStarknetContract<typeof daiJoinAbi>;
+export type SNJug = WrappedStarknetContract<typeof jugAbi>;
+
+export type Felt = bigint | string; // TODO: what it should be exactly
 
 // TODO: there should be global "active account" ala prank somewhere
 export async function deploySNVat(
@@ -25,3 +37,40 @@ export async function deploySNVat(
 // TODO: add more contracts
 // TODO: add init script
 // Based on: https://github.com/makerdao/dss-bridge/blob/4cfc84761b4bfeae747af14d3a2545377dd3304a/src/deploy/XDomainDss.sol
+export async function getDaiJoin(address: string): Promise<SNDaiJoin> {
+	const daiJoin = await getL2ContractAt(hre, "./abi/daiJoinAbi.ts", address);
+	return starknetPrank<typeof daiJoinAbi>(wrapTyped(hre, daiJoin));
+}
+
+export async function getDai(address: string): Promise<SNDai> {
+	const dai = await getL2ContractAt(hre, "./abi/daiAbi.ts", address);
+	return starknetPrank<typeof daiAbi>(wrapTyped(hre, dai));
+}
+
+export async function getVat(address: string): Promise<SNVat> {
+	const vat = await getL2ContractAt(hre, "./abi/vatAbi.ts", address);
+	return starknetPrank<typeof vatAbi>(wrapTyped(hre, vat));
+}
+
+export async function getJug(address: string): Promise<SNJug> {
+	const jug = await getL2ContractAt(hre, "./abi/jugAbi.ts", address);
+	return starknetPrank<typeof jugAbi>(wrapTyped(hre, jug));
+}
+
+export async function getCure(address: string): Promise<SNCure> {
+	const cure = await getL2ContractAt(hre, "./abi/cureAbi.ts", address);
+	return starknetPrank<typeof cureAbi>(wrapTyped(hre, cure));
+}
+
+export interface XDomainDssConfig {
+	claimToken: string;
+	endWait: SplitUintType<bigint>;
+}
+
+export interface SNDssInstance {
+	vat: SNVat;
+	jug: SNJug;
+	cure: SNCure;
+}
+
+export async function init(dss: SNDssInstance, cfg: XDomainDssConfig) {}
